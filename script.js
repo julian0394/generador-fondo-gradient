84cfb92a15cacb7de80f423034f3e65edeebc3d1
Julián <55754721+julian0394@users.noreply.github.com>
2021-01-19 16:56:18 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+var fondo = document.querySelector("body");         // Selecciono el body para manipular el fondo.
+var h3 = document.querySelector("h3");              // Selecciono el h3 para manipular el texto con los colores.
+var color1 = document.querySelector(".color1");     // Selecciono el control de color 1.
+var color2 = document.querySelector(".color2");     // Selecciono el control de color 2.
+
+color1.addEventListener("input", cambiarFondo);
+color2.addEventListener("input", cambiarFondo);
+
+//Cambia el color del fondo por el del color elegido
+function cambiarFondo() {
+    fondo.style.background = "linear-gradient(to right, " + color1.value + ", " + color2.value + ")";
+    crearCss();
+}
+
+//Cambia el texto con el del color elegido
+function crearCss() {
+    h3.textContent = "background: linear-gradient(to right, " + color1.value + ", " + color2.value + ");";
+}
